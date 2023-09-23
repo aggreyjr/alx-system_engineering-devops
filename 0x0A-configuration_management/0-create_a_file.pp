5f6c51fd69652b08b4a518703c8cf147ed71ca10
aggreyjr <tukashabaaggrey89@gmail.com>
2023-09-23 00:39:30 -0700

    second

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Create a file
+# create a file in /tmp with specific requirment
 
-file { '/tmp/holberton':
-  ensure  => file,
+file { 'holberton':
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
